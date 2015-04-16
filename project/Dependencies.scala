8ddc7de77055436349de711f3cdb65b985cfe2f0
Brian Harrington <brharrington@gmail.com>
2015-04-16 12:30:16 -0700

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,8 +8,8 @@ object Dependencies {
     val ribbon     = "2.0.0"
     val rxnetty    = "0.4.8"
     val scala      = "2.11.6"
-    val slf4j      = "1.7.10"
-    val spectator  = "0.20.0"
+    val slf4j      = "1.7.12"
+    val spectator  = "0.22.0"
   }
 
   import Versions._
@@ -20,7 +20,7 @@ object Dependencies {
   val archaiusPersist = "com.netflix.archaius" % "archaius-persisted2" % archaius
   val archaiusTypesafe= "com.netflix.archaius" % "archaius-typesafe" % archaius
   val equalsVerifier  = "nl.jqno.equalsverifier" % "equalsverifier" % "1.5.1"
-  val eureka          = "com.netflix.eureka" % "eureka-client" % "1.1.150"
+  val eureka          = "com.netflix.eureka" % "eureka-client" % "1.1.151"
   val governator      = "com.netflix.governator" % "governator" % "1.3.3"
   val guiceAssist     = "com.google.inject.extensions" % "guice-assistedinject" % guice
   val guiceCore       = "com.google.inject" % "guice" % guice
@@ -28,7 +28,7 @@ object Dependencies {
   val guiceMulti      = "com.google.inject.extensions" % "guice-multibindings" % guice
   val guiceServlet    = "com.google.inject.extensions" % "guice-servlet" % guice
   val inject          = "javax.inject" % "javax.inject" % "1"
-  val jodaTime        = "joda-time" % "joda-time" % "2.5"
+  val jodaTime        = "joda-time" % "joda-time" % "2.7"
   val karyonAdmin     = "com.netflix.karyon" % "karyon2-admin-web" % karyon
   val karyonCore      = "com.netflix.karyon" % "karyon2-core" % karyon
   val junit           = "junit" % "junit" % "4.10"
@@ -38,10 +38,10 @@ object Dependencies {
   val ribbonEureka    = "com.netflix.ribbon" % "ribbon-eureka" % ribbon
   val ribbonHttp      = "com.netflix.ribbon" % "ribbon-httpclient" % ribbon
   val ribbonLb        = "com.netflix.ribbon" % "ribbon-loadbalancer" % ribbon
-  val rxjava          = "io.reactivex" % "rxjava" % "1.0.8"
-  val rxnettyCore     = "io.reactivex" % "rxnetty" % "0.4.8"
-  val rxnettyCtxts    = "io.reactivex" % "rxnetty-contexts" % "0.4.8"
-  val rxnettySpectator= "io.reactivex" % "rxnetty-spectator" % "0.4.8"
+  val rxjava          = "io.reactivex" % "rxjava" % "1.0.9"
+  val rxnettyCore     = "io.reactivex" % "rxnetty" % rxnetty
+  val rxnettyCtxts    = "io.reactivex" % "rxnetty-contexts" % rxnetty
+  val rxnettySpectator= "io.reactivex" % "rxnetty-spectator" % rxnetty
   val scalaLibrary    = "org.scala-lang" % "scala-library" % scala
   val scalaLibraryAll = "org.scala-lang" % "scala-library-all" % scala
   val scalaLogging    = "com.typesafe.scala-logging" % "scala-logging_2.11" % "3.1.0"
