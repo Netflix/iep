a53e7fc2447146f433b993dd8aa0ac7604f962ad
Brent Pitman <bpitman@netflix.com>
2015-07-14 12:19:14 -0700

    Add release override back

--- project/Bintray.scala
+++ project/Bintray.scala
@@ -25,8 +25,7 @@ object Bintray {
       if (isSnapshot.value)
         Some("OJO" at s"https://oss.jfrog.org/oss-snapshot-local;timestamp=${now}/")
       else
-        publishTo.value
-        //Some("bintray" at s"https://api.bintray.com/maven/${bintrayOrganization.value.get}/${bintrayRepository.value}/maven/")
+        Some("bintray" at s"https://api.bintray.com/${bintrayOrganization.value.get}/${bintrayRepository.value}/")
     },
 
     storeBintrayCredentials := {
