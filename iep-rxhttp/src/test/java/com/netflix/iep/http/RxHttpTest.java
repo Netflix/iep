7d775f186f40e2b528232b4212a25dc661c1f2a3
Brent Pitman <bpitman@netflix.com>
2015-07-17 16:51:56 -0700

    Add log line

--- iep-rxhttp/src/test/java/com/netflix/iep/http/RxHttpTest.java
+++ iep-rxhttp/src/test/java/com/netflix/iep/http/RxHttpTest.java
@@ -262,6 +262,7 @@ System.out.println(contentEnc);
   }
 
   private void codeTest(final int code, final int attempts) throws Exception {
+System.out.println("STARTING codeTest: " + code + ", " + attempts);
     statusCode.set(code);
     AtomicIntegerArray expected = copy(statusCounts);
     expected.addAndGet(code, attempts);
