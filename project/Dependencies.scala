8d85b7b5b880aee78d277968c3b61ee5a592c662
Brian Harrington <brharrington@gmail.com>
2015-10-27 10:40:06 -0700

    eureka-client-1.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val archaiusPersist = "com.netflix.archaius" % "archaius2-persisted2" % archaius
   val archaiusTypesafe= "com.netflix.archaius" % "archaius2-typesafe" % archaius
   val equalsVerifier  = "nl.jqno.equalsverifier" % "equalsverifier" % "1.7.5"
-  val eureka          = "com.netflix.eureka" % "eureka-client" % "1.3.2"
+  val eureka          = "com.netflix.eureka" % "eureka-client" % "1.3.3"
   val governator      = "com.netflix.governator" % "governator-core" % "1.10.5"
   val guiceAssist     = "com.google.inject.extensions" % "guice-assistedinject" % guice
   val guiceCore       = "com.google.inject" % "guice" % guice
