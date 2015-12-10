c14a064232e430de4f4790f3cb1c075164a0a992
Brian Harrington <brharrington@gmail.com>
2015-12-09 22:15:13 -0800

    fix sync-init when loading dynamic config

---
+++ iep-platformservice/src/main/java/com/netflix/iep/platformservice/FixedPollingStrategy.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.iep.platformservice;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.netflix.archaius.config.PollingStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.archaius.util.Futures;
+import com.netflix.archaius.util.ThreadFactories;
+
+/**
+ * Originally from: https://github.com/Netflix/archaius/blob/2.x/archaius2-core/src/main/java/com/netflix/archaius/config/polling/FixedPollingStrategy.java
+ *
+ * Imported to fix https://github.com/Netflix/archaius/pull/370
+ */
+public class FixedPollingStrategy implements PollingStrategy {
+  private static final Logger LOG = LoggerFactory.getLogger(FixedPollingStrategy.class);
+
+  private final ScheduledExecutorService executor;
+  private final long interval;
+  private final TimeUnit units;
+  private final boolean syncInit;
+
+  public FixedPollingStrategy(long interval, TimeUnit units) {
+    this(interval, units, true);
+  }
+
+  public FixedPollingStrategy(long interval, TimeUnit units, boolean syncInit) {
+    this.executor = Executors.newSingleThreadScheduledExecutor(ThreadFactories.newNamedDaemonThreadFactory("Archaius-Poller-%d"));
+    this.interval = interval;
+    this.units    = units;
+    this.syncInit = syncInit;
+  }
+
+  @Override
+  public Future<?> execute(final Runnable callback) {
+    while (syncInit) {
+      try {
+        callback.run();
+        break;
+      }
+      catch (Exception e) {
+        try {
+          units.sleep(interval);
+        }
+        catch (InterruptedException e1) {
+          Thread.currentThread().interrupt();
+          return Futures.immediateFailure(e);
+        }
+      }
+    }
+    return executor.scheduleWithFixedDelay(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          callback.run();
+        } catch (Exception e) {
+          LOG.warn("Failed to load properties", e);
+        }
+      }
+    }, interval, interval, units);
+  }
+
+  @Override
+  public void shutdown() {
+    executor.shutdown();
+  }
+
+}
---
+++ iep-platformservice/src/main/java/com/netflix/iep/platformservice/PollingDynamicConfig.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.iep.platformservice;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.netflix.archaius.config.AbstractConfig;
+import com.netflix.archaius.config.PollingStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.archaius.config.polling.PollingResponse;
+
+/**
+ * Originally from: https://github.com/Netflix/archaius/blob/2.x/archaius2-core/src/main/java/com/netflix/archaius/config/PollingDynamicConfig.java
+ *
+ * Imported to fix https://github.com/Netflix/archaius/issues/371
+ *
+ * Special DynamicConfig that reads an entire snapshot of the configuration
+ * from a source and performs a delta comparison.  Each new snapshot becomes
+ * the new immutable Map backing this config.  
+ *
+ * @author elandau
+ *
+ */
+public class PollingDynamicConfig extends AbstractConfig {
+  private static final Logger LOG = LoggerFactory.getLogger(PollingDynamicConfig.class);
+
+  private volatile Map<String, String> current = new HashMap<String, String>();
+  private final AtomicBoolean busy = new AtomicBoolean();
+  private final Callable<PollingResponse> reader;
+  private final AtomicLong updateCounter = new AtomicLong();
+  private final AtomicLong errorCounter = new AtomicLong();
+  private final PollingStrategy strategy;
+
+  public PollingDynamicConfig(Callable<PollingResponse> reader, PollingStrategy strategy) {
+    this.reader = reader;
+    this.strategy = strategy;
+    strategy.execute(new Runnable() {
+      @Override
+      public void run() {
+        update();
+      }
+    });
+  }
+
+  @Override
+  public boolean containsKey(String key) {
+    return current.containsKey(key);
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return current.isEmpty();
+  }
+
+  @Override
+  public Object getRawProperty(String key) {
+    return current.get(key);
+  }
+
+  private void update() {
+    // OK to ignore calls to update() if already busy updating
+    if (busy.compareAndSet(false, true)) {
+      updateCounter.incrementAndGet();
+      try {
+        PollingResponse response = reader.call();
+        if (response.hasData()) {
+          current = response.getToAdd();
+          notifyConfigUpdated(this);
+        }
+      }
+      catch (Exception e) {
+        errorCounter.incrementAndGet();
+        try {
+          notifyError(e, this);
+        }
+        catch (Exception e2) {
+          LOG.warn("Failed to notify error observer", e2);
+        }
+        throw new RuntimeException(e);
+      }
+      finally {
+        busy.set(false);
+      }
+    }
+  }
+
+  public void shutdown() {
+    strategy.shutdown();
+  }
+
+  public long getUpdateCounter() {
+    return updateCounter.get();
+  }
+
+  public long getErrorCounter() {
+    return errorCounter.get();
+  }
+
+  @Override
+  public Iterator<String> getKeys() {
+    return current.keySet().iterator();
+  }
+}
