dd51514e3c61ae7ef7fe772765eb316f2da5b5c7
Brian Harrington <brharrington@gmail.com>
2016-01-18 10:16:24 -0800

    fix name of SpectatorMetricCollector

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val rxnetty    = "0.4.15"
     val scala      = "2.11.7"
     val slf4j      = "1.7.13"
-    val spectator  = "0.33.0"
+    val spectator  = "0.34.1"
   }
 
   import Versions._
