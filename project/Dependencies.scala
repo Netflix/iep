9f81f7ca1fda56806bffd462b00739b69cfd54fa
Brent Pitman <bpitman@netflix.com>
2016-03-11 10:02:34 -0800

    Pin to older archaius because newer not backward compatible

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val archaius   = "2.0.0"
+    val archaius   = "2.0.0-rc.33"
     val eureka     = "1.4.3"
     val guice      = "4.0"
     val karyon3    = "3.0.1-rc.20"
