e8ebdbf1cba5f16557f5103967ede1875f06b708
Brian Harrington <brharrington@gmail.com>
2016-07-30 15:31:23 -0700

    fix storeBintrayCredentials task
    Fixes the storeBintrayCredentials to work when the
    bintray plugin is disabled. This allows us to generate
    the file needed by the bintray plugin using the
    environment variables available in the travis build.

--- project/Bintray.scala
+++ project/Bintray.scala
@@ -1,7 +1,6 @@
 import sbt._
 import sbt.Keys._
 import bintray.BintrayPlugin._
-import bintray.BintrayCredentials.api
 import bintray.BintrayKeys._
 
 object Bintray {
@@ -16,8 +15,6 @@ object Bintray {
   lazy val user = get("User")
   lazy val pass = get("Key")
 
-  lazy val storeBintrayCredentials = taskKey[Unit]("store bintray credentials")
-
   lazy val settings: Seq[Def.Setting[_]] = bintraySettings ++ Seq(
     bintrayRepository := "maven",
     bintrayPackage := "iep",
@@ -31,10 +28,6 @@ object Bintray {
         Some("OJO" at s"https://oss.jfrog.org/oss-snapshot-local;build.timestamp=${now}/")
       else
         publishTo in bintray value
-    },
-
-    storeBintrayCredentials := {
-      IO.write(bintrayCredentialsFile.value, api.template(user, pass))
     },
 
     pomExtra := (
--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -21,6 +21,9 @@ object BuildSettings {
   lazy val checkLicenseHeaders = taskKey[Unit]("Check the license headers for all source files.")
   lazy val formatLicenseHeaders = taskKey[Unit]("Fix the license headers for all source files.")
 
+  lazy val storeBintrayCredentials = taskKey[Unit]("Store bintray credentials.")
+  lazy val credentialsFile = Path.userHome / ".bintray" / ".credentials"
+
   lazy val baseSettings =
     sbtrelease.ReleasePlugin.releaseSettings ++
       GitVersion.settings
@@ -37,8 +40,15 @@ object BuildSettings {
     fork in Test := true,
     autoScalaLibrary := false,
     externalResolvers := BuildSettings.resolvers,
+
     checkLicenseHeaders := License.checkLicenseHeaders(streams.value.log, sourceDirectory.value),
-    formatLicenseHeaders := License.formatLicenseHeaders(streams.value.log, sourceDirectory.value)
+    formatLicenseHeaders := License.formatLicenseHeaders(streams.value.log, sourceDirectory.value),
+
+    storeBintrayCredentials := {
+      IO.write(
+        credentialsFile,
+        bintray.BintrayCredentials.api.template(Bintray.user, Bintray.pass))
+    }
   )
 
   lazy val commonDeps = Seq(
@@ -71,8 +81,7 @@ object BuildSettings {
   // [warn] Credentials file /Users/brharrington/.bintray/.credentials does not exist
   // ```
   def bintrayProfile(p: Project): Project = {
-    val credsPath = Path.userHome / ".bintray" / ".credentials"
-    if (credsPath.exists)
+    if (credentialsFile.exists)
       p.settings(Bintray.settings)
     else
       p.disablePlugins(bintray.BintrayPlugin)
