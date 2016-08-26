175ca1dc588e974db47db26ace077cc6830f55c8
Brian Harrington <brharrington@gmail.com>
2016-08-26 05:32:11 -0700

    archaius 2.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val archaius   = "2.1.0"
+    val archaius   = "2.1.4"
     val aws        = "1.11.26"
     val eureka     = "1.4.11"
     val guice      = "4.1.0"
