bf71efa6c42442ed7b8b1afef278dd1f939bfecd
Brian Harrington <brharrington@gmail.com>
2016-09-24 13:29:12 -0700

    aws 1.11.37

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val archaius   = "2.1.5"
-    val aws        = "1.11.33"
+    val aws        = "1.11.37"
     val eureka     = "1.4.11"
     val guice      = "4.1.0"
     val jackson    = "2.8.2"
