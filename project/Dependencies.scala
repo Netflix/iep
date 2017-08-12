72ff04b7de04d7ee7a77f9c974475574c9ca5a47
brharrington <brharrington@gmail.com>
2017-08-11 22:35:59 -0700

    enable build for jdk9 (#328)
    Updates the travis config so that we build on
    both jdk8 and jdk9.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val jackson    = "2.8.7"
     val rxnetty    = "0.4.20"
     val rxscala    = "0.26.5"
-    val scala      = "2.11.11"
+    val scala      = "2.12.3"
     val slf4j      = "1.7.25"
     val spectator  = "0.57.1"
   }
