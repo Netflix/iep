82745ebe9ccf127657728f4e662896b40b5a6810
Brian Harrington <brharrington@gmail.com>
2018-01-18 07:28:57 -0800

    eureka 1.8.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val archaius   = "2.2.13"
     val aws        = "1.11.264"
     val aws2       = "2.0.0-preview-2"
-    val eureka     = "1.8.5"
+    val eureka     = "1.8.6"
     val guice      = "4.1.0"
     val jackson    = "2.9.1"
     val rxnetty    = "0.4.20"
