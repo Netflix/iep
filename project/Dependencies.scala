bd62ea9432886ff3e71ade9c3a17d0129faf2a52
Brian Harrington <brharrington@gmail.com>
2018-03-30 07:28:01 -0700

    rxjava1 1.3.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
   val junitInterface   = "com.novocode" % "junit-interface" % "0.11"
   val jzlib            = "com.jcraft" % "jzlib" % "1.1.3"
   val reactiveStreams  = "org.reactivestreams" % "reactive-streams" % "1.0.2"
-  val rxjava           = "io.reactivex" % "rxjava" % "1.3.6"
+  val rxjava           = "io.reactivex" % "rxjava" % "1.3.7"
   val rxjava2          = "io.reactivex.rxjava2" % "rxjava" % "2.1.9"
   val rxScala          = "io.reactivex" %% "rxscala" % rxscala
   val rxnettyCore      = "io.reactivex" % "rxnetty" % rxnetty
