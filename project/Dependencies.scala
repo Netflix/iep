0d18fc7404c25f1a3c490a7decdfaf57783b9573
Brian Harrington <brharrington@gmail.com>
2018-07-11 16:39:08 -0700

    aws-java-sdk 1.11.356

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val archaius   = "2.3.2"
-    val aws        = "1.11.339"
+    val aws        = "1.11.356"
     val aws2       = "2.0.0-preview-10"
     val eureka     = "1.9.2"
     val guice      = "4.1.0"
