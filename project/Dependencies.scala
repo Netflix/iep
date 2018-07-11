7e4eb0a1a6499e63470b17eabcc608e97cf0f32f
Brian Harrington <brharrington@gmail.com>
2018-07-11 16:41:52 -0700

    eureka-client 1.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val archaius   = "2.3.2"
     val aws        = "1.11.356"
     val aws2       = "2.0.0-preview-10"
-    val eureka     = "1.9.2"
+    val eureka     = "1.9.3"
     val guice      = "4.1.0"
     val jackson    = "2.9.6"
     val rxnetty    = "0.4.20"
