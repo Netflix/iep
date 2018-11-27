ff014ab13c5e61e6f8809103f32fadfb49467ffe
Brian Harrington <brharrington@gmail.com>
2018-11-27 15:24:14 -0800

    rxjava 2.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -68,7 +68,7 @@ object Dependencies {
   val jzlib            = "com.jcraft" % "jzlib" % "1.1.3"
   val reactiveStreams  = "org.reactivestreams" % "reactive-streams" % "1.0.2"
   val rxjava           = "io.reactivex" % "rxjava" % "1.3.8"
-  val rxjava2          = "io.reactivex.rxjava2" % "rxjava" % "2.2.3"
+  val rxjava2          = "io.reactivex.rxjava2" % "rxjava" % "2.2.4"
   val rxScala          = "io.reactivex" %% "rxscala" % rxscala
   val rxnettyCore      = "io.reactivex" % "rxnetty" % rxnetty
   val rxnettySpectator = "io.reactivex" % "rxnetty-spectator" % rxnetty
