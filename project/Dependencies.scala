0bc8b2a352988ea12a1cc1997208a1cf6e4876ef
Brian Harrington <brharrington@gmail.com>
2019-01-28 14:40:15 -0800

    scala 2.12.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val jackson    = "2.9.8"
     val rxnetty    = "0.4.20"
     val rxscala    = "0.26.5"
-    val scala      = "2.12.7"
+    val scala      = "2.12.8"
     val slf4j      = "1.7.25"
     val spectator  = "0.83.0"
   }
