87eb813e86b4ba7c7fa375b1918480b893d4e38c
Brian Harrington <brharrington@gmail.com>
2019-02-27 06:42:15 -0800

    slf4j 1.7.26

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val rxnetty    = "0.4.20"
     val rxscala    = "0.26.5"
     val scala      = "2.12.8"
-    val slf4j      = "1.7.25"
+    val slf4j      = "1.7.26"
     val spectator  = "0.83.0"
   }
 
