06eead8d78d4efcda3ff6c7fd82332b06ff103b3
Brian Harrington <brharrington@gmail.com>
2019-07-02 09:52:04 -0700

    eureka-client 1.9.12
    A new binding is needed as it now supports a custom
    endpoint resolver.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val archaius   = "2.3.14"
     val aws        = "1.11.556"
     val aws2       = "2.5.47"
-    val eureka     = "1.9.9"
+    val eureka     = "1.9.12"
     val guice      = "4.1.0"
     val jackson    = "2.9.9"
     val rxnetty    = "0.4.20"
