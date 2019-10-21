cb03a29752ac9903bc25577be13bc76f35afc60a
Brian Harrington <brharrington@gmail.com>
2019-10-21 13:28:46 -0700

    spectator 0.98.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val rxscala    = "0.26.5"
     val scala      = "2.12.8"
     val slf4j      = "1.7.28"
-    val spectator  = "0.96.0"
+    val spectator  = "0.98.0"
   }
 
   import Versions._
