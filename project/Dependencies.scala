80bd7ad6610c7b9c435b0374e30acff992df21dc
Brian Harrington <brharrington@gmail.com>
2020-06-01 11:33:52 -0500

    assertj 3.16.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val archaius   = "2.3.16"
-    val assertj    = "3.15.0"
+    val assertj    = "3.16.1"
     val aws        = "1.11.790"
     val aws2       = "2.13.26"
     val eureka     = "1.9.20"
