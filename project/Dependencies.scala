e8ae2c7a4c560acd427cdda68e84bfa7c4ea0172
Brian Harrington <brharrington@gmail.com>
2020-11-10 14:14:07 -0600

    eureka 1.10.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val assertj    = "3.17.2"
     val aws        = "1.11.892"
     val aws2       = "2.15.20"
-    val eureka     = "1.10.4"
+    val eureka     = "1.10.8"
     val graal      = "20.2.0"
     val guice      = "4.1.0"
     val jackson    = "2.11.3"
