ac6037005c7f0c2b791c1d3642c9544af4de57d9
Brian Harrington <brharrington@gmail.com>
2020-12-11 10:29:56 -0600

    jackson 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val eureka     = "1.10.8"
     val graal      = "20.2.0"
     val guice      = "4.1.0"
-    val jackson    = "2.11.3"
+    val jackson    = "2.12.0"
     val rxnetty    = "0.4.20"
     val rxscala    = "0.26.5"
     val scala      = "2.12.12"
