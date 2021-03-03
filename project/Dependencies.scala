8aee9211cfee6bac94c10bd96675e0994b61a961
Brian Harrington <brharrington@gmail.com>
2021-03-03 07:19:13 -0600

    graal 21.0.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val aws        = "1.11.965"
     val aws2       = "2.16.10"
     val eureka     = "1.10.12"
-    val graal      = "21.0.0"
+    val graal      = "21.0.0.2"
     val guice      = "4.1.0"
     val jackson    = "2.12.1"
     val rxnetty    = "0.4.20"
