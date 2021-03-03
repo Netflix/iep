c92355045f940b084419f5aa4e2d4ea3aa985b57
Brian Harrington <brharrington@gmail.com>
2021-03-03 07:13:43 -0600

    aws-sdk-java-v2 2.16.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val archaius   = "2.3.16"
     val assertj    = "3.19.0"
     val aws        = "1.11.965"
-    val aws2       = "2.15.74"
+    val aws2       = "2.16.10"
     val eureka     = "1.10.12"
     val graal      = "21.0.0"
     val guice      = "4.1.0"
