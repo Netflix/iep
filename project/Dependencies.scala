6928b387ebbf275b5ee83706ca6a1e1cd69bd7f0
Brian Harrington <brharrington@gmail.com>
2021-12-03 07:29:38 -0800

    aws-java-sdk 1.12.122

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val archaius   = "2.3.16"
     val assertj    = "3.21.0"
-    val aws        = "1.12.100"
+    val aws        = "1.12.122"
     val aws2       = "2.17.95"
     val graal      = "21.1.0"
     val guice      = "5.0.1"
