79677ad8d956ee1ad04f3216ef3355fd083c4de9
Brian Harrington <brharrington@gmail.com>
2022-09-01 10:20:52 -0500

    aws-java-sdk 1.12.294

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.librarymanagement.DependencyBuilders.OrganizationArtifactName
 object Dependencies {
   object Versions {
     val assertj    = "3.23.1"
-    val aws        = "1.12.272"
+    val aws        = "1.12.294"
     val aws2       = "2.17.243"
     val graal      = "21.1.0"
     val jackson    = "2.13.3"
