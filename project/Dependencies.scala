af232b58f0a5e2124bb2bd0d56896ccc740526a8
Brian Harrington <brharrington@gmail.com>
2023-02-28 19:17:21 -0600

    equalsverifier 3.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val aws2STS            = "software.amazon.awssdk" % "sts" % aws2
   val aws2UrlClient      = "software.amazon.awssdk" % "url-connection-client" % aws2
   val caffeine           = "com.github.ben-manes.caffeine" % "caffeine" % "2.9.3"
-  val equalsVerifier     = "nl.jqno.equalsverifier" % "equalsverifier" % "3.12.3"
+  val equalsVerifier     = "nl.jqno.equalsverifier" % "equalsverifier" % "3.14"
   val graalJs            = "org.graalvm.js" % "js" % graal
   val graalJsEngine      = "org.graalvm.js" % "js-scriptengine" % graal
   val jacksonCore        = "com.fasterxml.jackson.core" % "jackson-core" % jackson
