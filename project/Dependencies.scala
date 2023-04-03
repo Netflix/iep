c61244c7e5b463c77bb9934bf66c2c8ef5573e80
Brian Harrington <brharrington@gmail.com>
2023-04-03 09:58:32 -0500

    caffeine 3.1.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val aws2SES            = "software.amazon.awssdk" % "ses" % aws2
   val aws2STS            = "software.amazon.awssdk" % "sts" % aws2
   val aws2UrlClient      = "software.amazon.awssdk" % "url-connection-client" % aws2
-  val caffeine           = "com.github.ben-manes.caffeine" % "caffeine" % "2.9.3"
+  val caffeine           = "com.github.ben-manes.caffeine" % "caffeine" % "3.1.5"
   val equalsVerifier     = "nl.jqno.equalsverifier" % "equalsverifier" % "3.14.1"
   val jacksonCore        = "com.fasterxml.jackson.core" % "jackson-core" % jackson
   val jacksonMapper      = "com.fasterxml.jackson.core" % "jackson-databind" % jackson
