725f6fd3ea390462f6284c4dafc4e4dddd54fadd
Brian Harrington <brharrington@gmail.com>
2024-04-04 08:15:43 -0500

    spring 6.1.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val scala      = "2.12.19"
     val slf4j      = "1.7.36"
     val spectator  = "1.7.11"
-    val spring     = "6.1.4"
+    val spring     = "6.1.5"
   }
 
   import Versions._
