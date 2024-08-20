4395df32236bc4aabbbe67f4c35e55917ebdfd55
Brian Harrington <brharrington@gmail.com>
2024-08-20 15:34:10 -0500

    spectator 1.7.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val jackson    = "2.17.2"
     val scala      = "2.12.19"
     val slf4j      = "2.0.16"
-    val spectator  = "1.7.17"
+    val spectator  = "1.7.19"
     val spring     = "6.1.12"
   }
 
