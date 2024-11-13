ab62e7ed6211237cbe735978e28f5be7284faac0
Brian Harrington <brharrington@gmail.com>
2024-11-13 13:51:37 -0600

    jackson 2.18.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object Versions {
     val assertj    = "3.26.3"
     val aws2       = "2.29.11"
-    val jackson    = "2.18.0"
+    val jackson    = "2.18.1"
     val scala      = "2.12.19"
     val slf4j      = "2.0.16"
     val spectator  = "1.8.2"
