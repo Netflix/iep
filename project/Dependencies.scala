a4b572973013232e696eb6a829fc69511b11c319
Brian Harrington <brharrington@gmail.com>
2025-02-07 13:04:10 -0600

    assertj 3.27.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val assertj    = "3.27.2"
+    val assertj    = "3.27.3"
     val aws2       = "2.30.13"
     val jackson    = "2.18.2"
     val scala      = "2.12.19"
