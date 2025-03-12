e2f89e263713ef6adbc3babd7bb4dc671d073034
Brian Harrington <brharrington@gmail.com>
2025-03-12 11:56:54 -0500

    aws-sdk-java-v2 2.30.38

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val assertj    = "3.27.3"
-    val aws2       = "2.30.17"
+    val aws2       = "2.30.38"
     val jackson    = "2.18.2"
     val scala      = "2.12.19"
     val slf4j      = "2.0.16"
