e75ce7938df520aa8ef7d16b0ab24547f8861329
Brian Harrington <brharrington@gmail.com>
2025-09-09 17:53:56 -0500

    aws 2.33.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val assertj    = "3.27.3"
-    val aws2       = "2.32.16"
+    val aws2       = "2.33.5"
     val jackson    = "2.20.0"
     val scala      = "2.12.20"
     val slf4j      = "2.0.17"
