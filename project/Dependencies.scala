6090cebb47120604ef628270985c8775d435b611
Brian Harrington <brharrington@gmail.com>
2025-11-18 10:55:21 -0600

    spring-boot 3.5.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val slf4j      = "2.0.17"
     val spectator  = "1.9.2"
     val spring     = "6.2.13"
-    val springBoot = "3.5.6"
+    val springBoot = "3.5.7"
   }
 
   import Versions._
